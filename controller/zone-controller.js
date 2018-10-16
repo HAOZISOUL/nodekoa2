7315d4ddbc1f214d24ea4c5a3ccee10f269a163d
haodong.zhao <haodong.zhao@sihuatech.com>
2018-10-16 18:08:28 +0800

    control中query里无法获取ctx对象

--- controller/zone-controller.js
+++ controller/zone-controller.js
@@ -5,12 +5,20 @@ const query = require("../lib/mysql");
 
 class ZoneController {
 	async getZoneList(ctx){
-		ctx.body = "hello world zone controller...."
-		// query("select * from biz_zone", function(err, vals, fields){
-		// 	//ctx.body = `<h3 style='text-align:center; margin-top:50px'>${vals}</h3>`;	
-		// 	ctx.body = '保存成功'
-		// 	console.info("zone controller get data...........", vals);
-		// });
+		// ctx.body = "hello world zone controller...."
+		let position = "";
+		let data = query("select * from biz_zone", function(err, vals, fields){
+			// ctx.body = `<h3 style='text-align:center; margin-top:50px'>${vals[0].position}</h3>`;
+      // console.info("zone controller get data...........", vals[0]);
+      // position = vals[0].position
+			return vals;
+		});
+    ctx.body = {
+      code: 0,
+      status: 'Ok'
+    }
+    // console.log(data)
+    // ctx.body = `<h3>${data}</h3>`;
 	}
 
 }
